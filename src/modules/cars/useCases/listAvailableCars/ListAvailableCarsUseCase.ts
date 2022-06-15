cc1f59c602b92ff3b01af00f1468dfdf2a564902
Lucas Vieira <lucasvieirask8@hotmail.com.br>
2022-06-15 14:39:01 -0300

    test: criacao da lista de carros disponiveis, teste e refatoracao do repositorio de carros

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { Car } from "../../entities/Car";
+import { ICarsRepository } from "../../repositories/ICarsRepository";
+
+interface IRequest {
+  category_id?: string;
+  brand?: string;
+  name?: string;
+}
+
+@injectable()
+class ListAvailableCarsUseCase {
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
+
+  async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
+    const cars = await this.carsRepository.findAvailable(
+      brand,
+      category_id,
+      name
+    );
+
+    return cars;
+  }
+}
+
+export { ListAvailableCarsUseCase };
